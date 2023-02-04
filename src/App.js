a808799c0af0157072adca5477c077117bcd2203
knreano <112625935+knreano@users.noreply.github.com>
2023-02-04 20:39:30 +0800

    add crud functionality

--- src/App.js
+++ src/App.js
@@ -1,39 +1,92 @@
 import React, { useState, useEffect } from "react";
 import { db } from "./firebase-config";
-import { collection, getDocs } from "firebase/firestore";
+import {
+  collection,
+  onSnapshot,
+  doc,
+  addDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   const [notes, setNotes] = useState([]);
+  const [newTitle, setNewTitle] = useState("");
+  const [newText, setNewText] = useState("");
+
+  const onTitleChange = (event) => {
+    setNewTitle(event.target.value);
+  };
+  const onTextChange = (event) => {
+    setNewText(event.target.value);
+  };
+  const addNote = async (event) => {
+    event.preventDefault();
+    await addDoc(collection(db, "notes"), { title: newTitle, text: newText });
+  };
+  const deleteNote = async (event) => {
+    const noteDoc = doc(db, "notes", event.target.dataset.id);
+    await deleteDoc(noteDoc);
+  };
 
   useEffect(() => {
-    async function getNotes() {
-      const notesColRef = collection(db, "notes");
-      const data = await getDocs(notesColRef);
+    onSnapshot(collection(db, "notes"), (data) => {
       setNotes(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    }
-    getNotes();
+    });
   }, []);
 
   return (
-    <div>
-      <form className="addnote">
-      <input type="text" placeholder="title"></input>
-      <input type="text" placeholder="text"></input>
-        <button type="submit">Add Note</button>
-      </form>
-
-      {notes.map((note) => (
-        <Note key={note.id} note={note} />
-      ))}
+    <div className="container">
+      <Notes notes={notes} deleteNote={deleteNote} />
+      <Form
+        newTitle={newTitle}
+        newText={newText}
+        onTitleChange={onTitleChange}
+        onTextChange={onTextChange}
+        addNote={addNote}
+      />
     </div>
   );
 }
 
-function Note({ note }) {
+function Form({ addNote, newTitle, newText, onTitleChange, onTextChange }) {
   return (
-    <div className="note">
-      <h1>{note.title}</h1>
-      <p>{note.text}</p>
+    <form className="col-3" onSubmit={addNote}>
+      <input
+        type="text"
+        className="form-control mt-1"
+        placeholder="title"
+        value={newTitle}
+        onChange={onTitleChange}
+      />
+      <input
+        type="text"
+        className="form-control mt-1"
+        placeholder="text"
+        value={newText}
+        onChange={onTextChange}
+      />
+      <button type="submit" className="btn btn-primary mt-1">
+        Add Note
+      </button>
+    </form>
+  );
+}
+
+function Notes({ notes, deleteNote }) {
+  return (
+    <div className="row gx-5">
+      {notes.map((note) => {
+        return (
+          <div className="col-4 card p-0" key={note.id}>
+            <div className="card-body">
+              <button className="btn-close float-end" data-id={note.id} onClick={deleteNote} />
+              <h1>{note.title}</h1>
+              <p>{note.text}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
