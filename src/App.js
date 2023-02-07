3971a2863bf30d8ef41dad4cc1d2de11a8fb86eb
Keanu Reano <reanokeanu@gmail.com>
2023-02-07 20:06:16 +0800

    update note function

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   getDocs,
   addDoc,
   deleteDoc,
+  updateDoc,
   query,
   orderBy,
   Timestamp,
@@ -26,6 +27,7 @@ function App() {
     const q = query(collection(db, "notes"), orderBy("timestamp", "desc"));
     const snapshot = await getDocs(q);
     setNotes(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    console.log("get note");
   };
   const addNote = (event) => {
     event.preventDefault();
@@ -37,11 +39,32 @@ function App() {
     setNewTitle("");
     setNewText("");
     getNotes();
+    console.log("added note");
   };
   const deleteNote = (id) => {
     const noteDoc = doc(db, "notes", id);
     deleteDoc(noteDoc);
     setNotes(notes.filter((note) => (note.id === noteDoc.id ? null : note)));
+    console.log("deleted note");
+  };
+  const editNote = (event) => {
+    event.preventDefault();
+    const noteId = event.target.id.value;
+    const noteTitle = event.target.title.value;
+    const noteText = event.target.text.value;
+    const editedNote = {};
+    const existingNote = notes.find((note) => note.id === noteId);
+    if (existingNote.title !== noteTitle) {
+      editedNote.title = noteTitle;
+    }
+    if (existingNote.text !== noteText) {
+      editedNote.text = noteText;
+    }
+    if (Object.keys(editedNote).length === 0) {
+      return;
+    }
+    updateDoc(doc(db, "notes", noteId), editedNote);
+    console.log("updated note");
   };
 
   useEffect(() => {
@@ -57,7 +80,7 @@ function App() {
         onTextChange={onTextChange}
         addNote={addNote}
       />
-      <Notes notes={notes} deleteNote={deleteNote} />
+      <Notes notes={notes} deleteNote={deleteNote} editNote={editNote} />
     </div>
   );
 }
@@ -66,33 +89,34 @@ function Form({ addNote, newTitle, newText, onTitleChange, onTextChange }) {
   return (
     <div>
       <form onSubmit={addNote}>
-        <input
-          type="text"
-          placeholder="title"
-          value={newTitle}
-          onChange={onTitleChange}
-        />
-        <input
-          type="text"
-          placeholder="text"
-          value={newText}
-          onChange={onTextChange}
-        />
-        <button type="submit">Add Note</button>
+        <input placeholder="title" value={newTitle} onChange={onTitleChange} />
+        <input placeholder="text" value={newText} onChange={onTextChange} />
+        <button type="submit">add Note</button>
       </form>
     </div>
   );
 }
 
-function Notes({ notes, deleteNote }) {
+function Notes({ notes, deleteNote, editNote }) {
   return (
     <div>
       {notes.map((note) => {
         return (
           <div key={note.id}>
             <button onClick={() => deleteNote(note.id)}>x</button>
-            <h6>{note.title}</h6>
-            <p>{note.text}</p>
+            <form onSubmit={editNote}>
+              <input name="id" type="hidden" value={note.id} />
+              <input
+                name="title"
+                defaultValue={note.title}
+                onBlur={(e) => e.target.form.requestSubmit()}
+              />
+              <input
+                name="text"
+                defaultValue={note.text}
+                onBlur={(e) => e.target.form.requestSubmit()}
+              />
+            </form>
           </div>
         );
       })}
