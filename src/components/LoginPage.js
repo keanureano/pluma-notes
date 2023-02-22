e543ac9883d17733569a90388460ac22c65c8925
Keanu Reano <reanokeanu@gmail.com>
2023-02-22 15:33:24 +0800

    design login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,8 +1,11 @@
 export default function LoginPage({ loginUser }) {
   return (
-    <>
-      <h1>Welcome to Pluma</h1>
-      <button onClick={loginUser}>Sign In With Google</button>
-    </>
+    <div className="d-flex h-100 justify-content-center align-items-center text-center">
+      <div>
+        <h1 className="display-1 mb-0">Welcome to</h1>
+        <h1 className="display-1 fw-semibold mb-4">Pluma</h1>
+        <button className="btn btn-dark" onClick={loginUser}>Sign In With Google</button>
+      </div>
+    </div>
   );
 }
